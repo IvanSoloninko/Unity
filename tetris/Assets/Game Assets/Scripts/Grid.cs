2810a9026c7b6161c4ba3e1027de96b35f599ad9
IvanSoloninko <webmanywani@gmail.com>
2020-12-17 12:49:46 +0200

    project

---
+++ tetris/Assets/Game Assets/Scripts/Grid.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Цей клас обробляє всю логіку щодо видалення рядків, коли користувач
+// отримано повний рядок з блоками. А також властивість Grid.grid
+// зберігає на матриці стан гри, якщо в заданому положенні (x, y)
+// є блок чи ні.
+
+public class Grid : MonoBehaviour {
+
+    // Сама сітка
+    public static int w = 10;
+    public static int h = 20;
+   // сітка, що зберігає елемент Transform
+    public static Transform[,] grid = new Transform[w, h];
+
+  // перетворюємо дійсний вектор у дискретні координати за допомогою Mathf.Round
+    public static Vector2 roundVector2(Vector2 v) {
+        return new Vector2 (Mathf.Round (v.x), Mathf.Round (v.y));
+    }
+
+   // перевіряємо, чи є якийсь вектор у межах гри (межі ліворуч, праворуч та вниз)
+    public static bool insideBorder(Vector2 pos) {
+        return ((int)pos.x >= 0 &&
+                (int)pos.x < w &&
+                (int)pos.y >= 0);
+    }
+
+    // знищуємо рядок у рядку y
+    public static void deleteRow(int y) {
+        for (int x = 0; x < w; x++) {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+
+   // Щоразу, коли рядок видалявся, вищезазначені рядки повинні падати внизу на одну одиницю.
+     // Наступна функція подбає про це:
+    public static void decreaseRow(int y) {
+        for (int x = 0; x < w; x++) {
+            if (grid[x, y] != null) {
+               // рухаємось одним донизу
+                grid[x, y - 1] = grid[x, y];
+                grid[x,y] = null;
+
+               // Оновлення позиції блоку
+                grid[x, y-1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+   // щоразу, коли рядок видаляється, усі вищезазначені рядки слід зменшувати на 1
+    public static void decreaseRowAbove(int y) {
+        for (int i = y; i < h; i++) {
+            decreaseRow(i);
+        }
+    }
+
+  // перевіряємо, чи рядок заповнений, а потім може бути видалений (оцінка +1)
+    public static bool isRowFull(int y){
+        for (int x = 0; x < w; x++) {
+            if (grid[x, y] == null) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void deleteFullRows() {
+        for (int y = 0; y < h; y++) {
+            if (isRowFull(y)) {
+                deleteRow(y);
+                decreaseRowAbove(y + 1);
+               // додаємо нові бали, щоб набрати, коли рядок буде видалено
+                ScoreManager.score += (h - y) * 10;
+                --y;
+              
+            }
+        }
+    }
+
+
+
+}
---
+++ tetris/Assets/Game Assets/Scripts/Group.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Group : MonoBehaviour {
+
+   // час останнього випадання елемента, що використовується для автоматичного падіння після
+     // час, параметризований за `рівнем`
+    private float lastFall;
+
+  // час останнього натискання клавіші для обробки поведінки тривалого натискання
+    private float lastKeyDown;
+    private float timeKeyPressed;
+
+    public void AlignCenter() {
+        transform.position += transform.position - Utils.Center(gameObject);
+    }
+
+
+    bool isValidGridPos() {
+        foreach (Transform child in transform) {
+            Vector2 v = Grid.roundVector2(child.position);
+
+            // не всередині кордону?
+            if(!Grid.insideBorder(v)) {
+                return false;
+            }
+
+           // Блок в комірці сітки ?
+            if (Grid.grid[(int)(v.x), (int)(v.y)] != null &&
+                Grid.grid[(int)(v.x), (int)(v.y)].parent != transform) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+ // оновлення сітки
+    void updateGrid() {
+       // Видалити старі обєкти із сітки
+        for (int y = 0; y < Grid.h; ++y) {
+            for (int x = 0; x < Grid.w; ++x) {
+                if (Grid.grid[x,y] != null &&
+                    Grid.grid[x,y].parent == transform) {
+                    Grid.grid[x,y] = null;
+                }
+            } 
+        }
+
+        insertOnGrid();
+    }
+
+    void insertOnGrid() {
+       // додаємо нових обєктів до сітки
+        foreach (Transform child in transform) {
+            Vector2 v = Grid.roundVector2(child.position);
+            Grid.grid[(int)v.x,(int)v.y] = child;
+        }
+    }
+
+    void gameOver() {
+        Debug.Log("GAME OVER!");
+        while (!isValidGridPos()) {
+          
+            transform.position  += new Vector3(0, 1, 0);
+        } 
+        updateGrid(); // щоб не перекривати недійсні групи
+        enabled = false; // вимкнути скрипт при смерті
+        UIController.gameOver(); // активна панель Game Over
+        Highscore.Set(ScoreManager.score); // встановити рекорд
+        
+    }
+
+   // Використовуй це для ініціалізації
+    void Start () {
+        lastFall = Time.time;
+        lastKeyDown = Time.time;
+        timeKeyPressed = Time.time;
+        if (isValidGridPos()) {
+            insertOnGrid();
+        } else { 
+            Debug.Log("KILLED ON START");
+            gameOver();
+        }
+
+    }
+
+    void tryChangePos(Vector3 v) {
+        
+        
+        transform.position += v;
+
+        // See if valid
+        if (isValidGridPos()) {
+            updateGrid();
+        } else {
+            transform.position -= v;
+        }
+    }
+
+    void fallGroup() {
+       // модифікувати
+        transform.position += new Vector3(0, -1, 0);
+
+        if (isValidGridPos()){
+           // Дійсний. Оновити сітку ... ще раз
+            updateGrid();
+        } else {
+           // це недійсне. повернути
+            transform.position += new Vector3(0, 1, 0);
+
+            // Очистити заповнені горизонтальні лінії
+            Grid.deleteFullRows();
+
+            // Вимкнути сценарій
+            FindObjectOfType<Spawner>().spawnNext();
+
+
+            
+            enabled = false;
+        }
+
+        lastFall = Time.time;
+
+    }
+
+   // getKey, якщо натиснути зараз на довше натиснути на 0,5 секунди | якщо це істина, застосовуйте клавішу кожного 0,05f під час натискання
+    bool getKey(KeyCode key) {
+        bool keyDown = Input.GetKeyDown(key);
+        bool pressed = Input.GetKey(key) && Time.time - lastKeyDown > 0.5f && Time.time - timeKeyPressed > 0.05f;
+
+        if (keyDown) {
+            lastKeyDown = Time.time;
+        }
+        if (pressed) {
+            timeKeyPressed = Time.time;
+        }
+ 
+        return keyDown || pressed;
+    }
+
+
+   // Оновлення викликається один раз на кадр
+    void Update () {
+        if (UIController.isPaused) {
+            return; // нічого не робити
+        }
+        if (getKey(KeyCode.LeftArrow)) {
+            tryChangePos(new Vector3(-1, 0, 0));
+        } else if (getKey(KeyCode.RightArrow)) {  // Рухатися вправо
+            tryChangePos(new Vector3(1, 0, 0));
+        } else if (getKey(KeyCode.UpArrow) && gameObject.tag != "Cube") { // Обертати
+            transform.Rotate(0, 0, -90);
+
+         // перевірити, чи дійсно
+            if (isValidGridPos()) {
+             // він дійсний. Оновити сітку
+                updateGrid();
+            } else {
+               // це недійсне. повернути
+                transform.Rotate(0, 0, 90);
+            }
+        } else if (getKey(KeyCode.DownArrow) || (Time.time - lastFall) >= (float)1 / Mathf.Sqrt(LevelManager.level)) {
+            fallGroup();
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            while (enabled) {  // падати до дна
+                fallGroup();
+            }
+        }
+
+    }
+}
